58ea1b80befdf62343d638ff122a4b5d853559ef
Siindu <saya60764@gmail.com>
2025-12-08 15:17:57 +0800

    Tambahin izin transaksi

--- contracts/Siky.sol
+++ contracts/Siky.sol
@@ -6,24 +6,45 @@ contract Siky {
   string public name = "Siky Coin";
   string public symbol = "SIKY";
   uint public decimals = 18;
-  uint public  totalSuply; 
+  uint public  totalSuply;
 
   mapping(address => uint) public balances;
 
   event Transfer(address indexed from, address indexed to, uint amount);
 
   constructor() {
+    owner = msg.sender;
+    paused = false;
     totalSuply =  1000000 * (10 ** uint(decimals));
     balances[msg.sender] = totalSuply;
     emit Transfer(address(0), msg.sender, totalSuply);
   }
 
+  // hanya pemilik yang bisa melakukan transaksi
+  modifier onlyOwner() {
+    require(msg.sender == owner, "You don't have permission to do this transaction!");
+    _;
+  }
+
+  modifier notPaused() {
+    require(paused == false, "This transaction paused");
+    _;
+  }
+
+  function pause() public onlyOwner {
+    paused = true;
+  }
+
+  function unpause() public onlyOwner {
+    paused = false;
+  }
+
   // cek saldo
-  function balanceOf(address account) public view returns (uint) {
+  function balanceOf(address account) public notPaused returns (uint) {
     return balances[account];
   }
 
-  function transfer(address to, uint amount) public returns (bool) {
+  function transfer(address to, uint amount) public notPaused returns (bool) {
     require(balances[msg.sender] >= amount, "Your balance is insufficient to make this transaction");
 
     balances[msg.sender] -= amount;
